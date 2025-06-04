74bced76b925708f27002614635000776a7a4352
GabiAmaia <gabicrystal013@gmail.com>
2025-06-04 15:19:47 -0300

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,8 @@
+numero = int(input('Digite um número. '))
+contador = 1
+fim = 10
+while contador <= fim:
+   # print(f'{contador} X {numero}')
+    print(f'{numero} X {contador} = {contador*numero}')
+    contador = contador + 1
+   
