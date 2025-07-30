0431e94d46497065f18bbbcc70694353a26c4928
GabiAmaia <gabicrystal013@gmail.com>
2025-07-30 13:15:10 -0300

    Add files via upload

---
+++ Cadastro.py
@@ -0,0 +1,9 @@
+print('Cadastro')
+nome = input('Digite seu nome: ')
+email = input('Digite seu email: ')
+senha = input('Digite sua senha: ')
+
+print('Cadastrado')
+print('Nome:' , nome)
+print('Email:' , email)
+print('Senha' , senha)
