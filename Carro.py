2b66352d3510a6bc98bcaf88ec6796f625819f8e
GabiAmaia <gabicrystal013@gmail.com>
2025-07-30 13:16:00 -0300

    Add files via upload

---
+++ Carro.py
@@ -0,0 +1,7 @@
+carro = input('Digite o nome do carro: ')
+valor = input('Digite o valor do carro: ')
+consumo = input('Digite o consumo KM por litro: ')
+
+print('Carro:' , carro) 
+print('Valor:' , valor)
+print('Consumo:' , consumo) 
