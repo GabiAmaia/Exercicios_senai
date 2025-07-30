2a4657d9ac53f4cf2462c277dda7f556a92ea406
GabiAmaia <gabicrystal013@gmail.com>
2025-07-30 13:13:08 -0300

    Add files via upload

---
+++ somador.py
@@ -0,0 +1,9 @@
+contador = 0
+fim = int(input('Digite um número para somar. '))
+soma = 0
+while contador <= fim:
+    soma = soma+contador
+    contador = contador+1
+
+print(f'Resultado: {soma}')
+
