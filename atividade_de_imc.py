1e4bba4b81f59d712371dd3dfd7381ea01ce014f
GabiAmaia <gabicrystal013@gmail.com>
2025-07-30 13:14:15 -0300

    Add files via upload

---
+++ atividade_de_imc.py
@@ -0,0 +1,9 @@
+
+peso= float(input ('peso do aluno:'))
+altura= float(input ('altura do aluno:'))
+imc = peso /(altura*altura) # round (expressão, quantidade de casas depois da virgula)
+
+
+resultado= round (imc,2)
+
+print(resultado)
